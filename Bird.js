180a15377824d758bd25feb899500864f003870a
umair8859 <mohsina.sachora@gmai.com>
2020-04-21 14:52:15 +0530

    trajactory

--- Bird.js
+++ Bird.js
@@ -2,12 +2,27 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
-  }
+    this.smoke=loadImage("sprites/smoke.png");
+    this.trajactory=[];
 
+  }
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
 
     super.display();
+    if(this.body.velocity.x>10&&this.body.position.x>200){
+
+     
+
+    
+    var position=[this.body.position.x,this.body.position.y];
+    this.trajactory.push(position);}
+   for(var i=0;i<this.trajactory.length;i=i+1){
+
+    image(this.smoke,this.trajactory[i][0],this.trajactory[i][1]);
+
+   }
+   
   }
 }
